c84298f4b30a1d216a38392619c036d2af181bd2
monchi316 <monchi316@gmail.com>
2020-06-01 18:39:05 +0900

    課題①-4完了

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,20 @@
 'user strict'
+
+const tasks = [{ content: '机を片付ける', genre: '掃除' }, { content: '牛乳を買う', genre: '買い物' }, { content: '散歩する', genre: '運動' }];
+
+function task() {
+  console.log('========================');
+  console.log('現在持っているタスク');
+  console.log('========================');
+
+  for (let i = 0; i < tasks.length; i++) {
+    console.log(`${i}:[内容]${tasks[i].content}、[ジャンル]${tasks[i].genre}`);
+  }
+}
+
+task();
+
+tasks.push({ content: prompt('内容を入力して下さい'), genre: prompt('ジャンルを入力して下さい') });
+alert('タスクを追加しました');
+
+task();
